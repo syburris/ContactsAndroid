f9edabb26a3408270bf3e0b014342a81c5a84bc2
Steven Burris <stevenburris.jr@gmail.com>
2016-09-28 16:22:51 -0400

    Initial import

---
+++ app/src/main/java/com/theironyard/contactsandroid/Contact.java
@@ -0,0 +1,36 @@
+package com.theironyard.contactsandroid;
+
+/**
+ * Created by stevenburris on 9/28/16.
+ */
+
+public class Contact {
+    String name;
+    String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + phoneNumber + ")";
+    }
+}
---
+++ app/src/main/java/com/theironyard/contactsandroid/MainActivity.java
@@ -0,0 +1,41 @@
+package com.theironyard.contactsandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ListView list;
+    EditText name;
+    EditText phoneNumber;
+    Button add;
+    ArrayAdapter<Contact> contacts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list = (ListView) findViewById(R.id.list);
+        name = (EditText) findViewById(R.id.name);
+        phoneNumber = (EditText) findViewById(R.id.number);
+        add = (Button) findViewById(R.id.button);
+
+        add.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String contactName = name.getText().toString();
+        String contactNumber = phoneNumber.getText().toString();
+        Contact contact = new Contact(contactName, contactNumber);
+        contacts.add(contact);
+
+    }
+}
